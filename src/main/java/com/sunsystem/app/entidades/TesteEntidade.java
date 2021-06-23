e0fae98d14519f06bbb8ba93d8146e7e0ce7b253
adesdsilva <adesdsilva@gmail.com>
2021-06-22 23:35:19 -0300

    my-app commit inicial

---
+++ src/main/java/com/sunsystem/app/entidades/TesteEntidade.java
@@ -0,0 +1,17 @@
+package com.sunsystem.app.entidades;
+
+public class TesteEntidade {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
